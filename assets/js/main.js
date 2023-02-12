2bfcd7eb16ad50f2e6b449493fcc19f6dce943c5
azimjonking <azimjonjalilov1301@mail.ru>
2023-02-12 21:26:12 +0200

    twam sec is done

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+
+/*================ TEAM SWIPER ================== */
+
+var testimonial__swiper = new Swiper(".testimonials__swiper", {
+    slidesPerView: "auto",
+    centerSlides: true,
+    spaceBetween: 32,
+    loop: true,
+});
